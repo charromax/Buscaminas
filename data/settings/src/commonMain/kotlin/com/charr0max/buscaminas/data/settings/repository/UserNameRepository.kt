d4b66482c3e88a695e523cf7c496d9630db96291
Manuel <58895379+charromax@users.noreply.github.com>
2025-01-30 15:40:28 -0300

    asdasdasd (#6)
    * FIX: dummy change
    
    * FIX: culengue
    
    ---------
    
    Co-authored-by: Manuel Gonzalez <mrgonzalez@applaudostudios.com>

--- data/settings/src/commonMain/kotlin/com/charr0max/buscaminas/data/settings/repository/UserNameRepository.kt
+++ data/settings/src/commonMain/kotlin/com/charr0max/buscaminas/data/settings/repository/UserNameRepository.kt
@@ -7,5 +7,5 @@ interface UserNameRepository {
     val userName: Flow<String>
     suspend fun updateUserName(name: String)
 
-    data object UserNameKey: Storage.Key.StringKey("username", "culo")
+    data object UserNameKey: Storage.Key.StringKey("username", "CULENGUE")
 }
